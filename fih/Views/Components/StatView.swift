974349e1a4045fc5b5766aa0e6b7e720307658c1
Mas Bowo <C14220311@john.petra.ac.id>
2026-05-16 00:49:22 +0700

    fix: add back icon using asset

---
+++ fih/Views/Components/StatView.swift
@@ -0,0 +1,18 @@
+//
+//  StatView.swift
+//  fih
+//
+//  Created by Satriya Handha Wibowo on 16/05/26.
+//
+
+import SwiftUI
+
+struct StatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatView()
+}
