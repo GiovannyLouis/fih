35c5035fe8d06c2d49366fb808c8be954a71e5e8
Mas Bowo <C14220311@john.petra.ac.id>
2026-05-18 23:35:33 +0700

    feat: create fish model and controller

---
+++ fih/Controllers/FishController.swift
@@ -0,0 +1,7 @@
+//
+//  FishController.swift
+//  fih
+//
+//  Created by Satriya Handha Wibowo on 18/05/26.
+//
+
---
+++ fih/Models/Fish.swift
@@ -0,0 +1,29 @@
+//
+//  Fish.swift
+//  fih
+//
+//  Created by Satriya Handha Wibowo on 18/05/26.
+//
+
+import SwiftData
+import Foundation
+
+
+@Model
+class Fish {
+    @Attribute(.unique) var id: UUID
+    var zone: Int
+    var name: String
+    var info: String // Hindari menggunakan nama variabel 'description' karena sering bentrok dengan bawaan Swift
+    var iconName: String
+    var isUnlocked: Bool
+    
+    init(id: UUID = UUID(), zone: Int, name: String, info: String, iconName: String, isUnlocked: Bool = false) {
+        self.id = id
+        self.zone = zone
+        self.name = name
+        self.info = info
+        self.iconName = iconName
+        self.isUnlocked = isUnlocked
+    }
+}
