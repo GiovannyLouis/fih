f207759afb83d546944c4b460590518c38207b65
Giovanny Louis <louis.anth10@gmail.com>
2026-05-22 17:35:28 +0700

    feat: finished predator animation

--- fih/Controllers/InGameController.swift
+++ fih/Controllers/InGameController.swift
@@ -168,7 +168,7 @@ class InGameController {
     
     private func spawnEvent() {
         let roll = Double.random(in: 0...1)
-        if roll < 0.1 {
+        if roll < 0.7 {
             spawnfish()
         } else {
             triggerObstacle()
@@ -198,7 +198,7 @@ class InGameController {
         guard !isExpeditionOver else { return }
                 
         // 80/20 Roll for Weather vs General Obstacle
-        let isWeatherSpecific = Double.random(in: 0...1) <= 0.10
+        let isWeatherSpecific = Double.random(in: 0...1) <= 0.80
         var chosenObstacleType: ObstacleType
         
         if isWeatherSpecific {
