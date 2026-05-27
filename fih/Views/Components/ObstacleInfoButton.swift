c3093a11e71168568415ad8be0191643f44796d5
Jose Taneo <joseperdanat@gmail.com>
2026-05-27 23:43:13 +0700

    feat: add blinking and appear animation if current days more than 2

---
+++ fih/Views/Components/ObstacleInfoButton.swift
@@ -0,0 +1,18 @@
+//
+//  ObstacleInfoButton.swift
+//  fih
+//
+//  Created by Jose Putra Perdana Taneo on 27/05/26.
+//
+
+import SwiftUI
+
+struct ObstacleInfoButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObstacleInfoButton()
+}
